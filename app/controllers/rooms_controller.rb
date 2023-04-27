16f5f8618fe4b276b4e1a78beee31d27be03fe83
Colleen <colleenrust524@EMAIL.com>
2023-04-27 14:16:54 -0500

    new and create methods for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,24 @@ class RoomsController < ApplicationController
     @room = Room.find_by(id: params[:id])
     render :show
   end
+  def new    
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(
+      user_id: params[:room][:user_id],
+      address: params[:room][:address],
+      city: params[:room][:city],
+      state: params[:room][:state],
+      price: params[:room][:price],
+      description: params[:room][:description],
+      home_type: params[:room][:home_type],
+      total_occupancy: params[:room][:total_occupancy],
+      total_bedrooms: params[:room][:total_bedrooms],
+      total_bathrooms: params[:room][:total_bathrooms]
+    )
+    @room.save
+    redirect_to "/rooms/#{@room.id}"
+  end
 end
