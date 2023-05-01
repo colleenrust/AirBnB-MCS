c877b0b788e4ff9badc3858a4fc6ba416a85fac6
Mattias Schade <mattiasschade@gmail.com>
2023-05-01 12:41:20 -0500

    Reservations price and total, Create new review

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  def price
+    room.price
+  end
+
+  def total
+    room.price * 1.09
+  end
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :reservation
-  belongs_to :user, through: :reservation
+  
+  
 end
